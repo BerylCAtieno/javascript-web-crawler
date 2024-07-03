9b674fc609af25d4396cbdc322a2bf3eca7471d3
BerylCAtieno <berylatieno30@gmail.com>
2024-07-03 19:02:36 +0300

    add getURLsFromHTML + tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,27 @@
+const { JSDOM }  = require('jsdom')
+
+function getURLsFromHTML(html, baseURL) {
+    const urls = []
+    const dom = new JSDOM(html)
+    const anchors = dom.window.document.querySelectorAll('a')
+  
+    for (const anchor of anchors) {
+      if (anchor.hasAttribute('href')) {
+        let href = anchor.getAttribute('href')
+  
+        try {
+          // convert any relative URLs to absolute URLs
+          href = new URL(href, baseURL).href
+          urls.push(href)
+        } catch(err) {
+          console.log(`${err.message}: ${href}`)
+        }
+      }
+    }
+  
+    return urls
+  }
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -9,4 +33,6 @@ function normalizeURL(urlString) {
     }
 }
 
-export { normalizeURL };
+module.exports = { getURLsFromHTML, 
+        normalizeURL
+ };
